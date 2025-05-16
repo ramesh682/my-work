ca7a8977c429af4ccac1fee813eddfc966e6aa9a
ramesh682 <rameshmalladi0@gmail.com>
2025-05-16 11:37:46 +0530

    Add files via upload

---
+++ variables and data types.py
@@ -0,0 +1,12 @@
+# Define variables with different data types
+user_id: int = 101
+user_name: str = "Ramesh"
+account_balance: float = 1500.75
+is_active: bool = True
+
+# Displaying variable values
+print(f"User ID: {user_id}")
+print(f"User Name: {user_name}")
+print(f"Account Balance: ₹{account_balance}")
+print(f"Is Account Active? {is_active}")
+
