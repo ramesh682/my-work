709e425cae8c80640ad8cfd2d67ca81018bcb869
ramesh682 <rameshmalladi0@gmail.com>
2025-05-16 11:44:30 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,13 @@
+#for loop
+# Print all even numbers from 1 to 10
+for num in range(1, 11):
+    if num % 2 == 0:
+        print(f"{num} is even")
+
+
+#while loop
+# Count down from 5 to 1
+counter = 5
+while counter > 0:
+    print(f"Countdown: {counter}")
+    counter -= 1  # same as counter = counter - 1
